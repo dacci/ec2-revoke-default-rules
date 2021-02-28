c4bd525b8b2cd13f52395e27f2f1f2cfeae157f4
Shinya Tsuda <shinya@dacci.org>
2021-02-28 21:18:41 +0900

    Added CDK application.

---
+++ cdk/index.ts
@@ -0,0 +1,63 @@
+import { NodejsFunction } from '@aws-cdk/aws-lambda-nodejs';
+import { App, CfnOutput, Duration, RemovalPolicy, Stack } from '@aws-cdk/core';
+import { Runtime } from '@aws-cdk/aws-lambda';
+import { PolicyStatement } from '@aws-cdk/aws-iam';
+import { LogGroup, RetentionDays } from '@aws-cdk/aws-logs';
+
+const app = new App();
+
+const stack = new Stack(app, 'Stack', {
+  stackName: 'ec2-revoke-default-rules',
+});
+
+const handler = new NodejsFunction(stack, 'Handler', {
+  entry: 'src/index.ts',
+  runtime: Runtime.NODEJS_14_X,
+  bundling: {
+    minify: true,
+    sourceMap: true,
+  },
+  timeout: Duration.minutes(1),
+  environment: {
+    NODE_OPTIONS: '--enable-source-maps',
+  },
+});
+
+handler.configureAsyncInvoke({
+  maxEventAge: Duration.minutes(1),
+  retryAttempts: 0,
+});
+
+handler.addToRolePolicy(new PolicyStatement({
+  actions: [
+    'ec2:DescribeSecurityGroups',
+  ],
+  resources: [
+    '*',
+  ],
+}));
+
+handler.addToRolePolicy(new PolicyStatement({
+  actions: [
+    'ec2:RevokeSecurityGroupIngress',
+    'ec2:RevokeSecurityGroupEgress',
+  ],
+  resources: [
+    stack.formatArn({
+      service: 'ec2',
+      resource: 'security-group',
+      resourceName: '*',
+    }),
+  ],
+}));
+
+new CfnOutput(stack, 'ServiceToken', {
+  value: handler.functionArn,
+  exportName: `${stack.stackName}:ServiceToken`,
+});
+
+new LogGroup(handler, 'LogGroup', {
+  logGroupName: `/aws/lambda/${handler.functionName}`,
+  retention: RetentionDays.ONE_WEEK,
+  removalPolicy: RemovalPolicy.DESTROY,
+});
